50d6a47415e65986f20202937f9c83308a7c7ec4
LAPTOP-RK1BUFU5\Indirapratibha <s_indirapratibha@u.nus.edu>
2026-01-28 00:09:56 +0800

    Renamed Duke.java to Vinux.java and implemented Level-0

---
+++ src/main/java/Vinux.java
@@ -0,0 +1,22 @@
+public class Vinux {
+    public static void main(String[] args) {
+        String logo = "           ________   __                         \n"
+                + "|\\      /| \\__  __/  ( (    /| |\\      /| |\\      /|\n"
+                + "| )    ( |    ) (    |  \\  ( | | )    ( | ( \\    / )\n"
+                + "| |    | |    | |    |   \\ | | | |    | |  \\ (__) /\n"
+                + "( (    ) )    | |    | (\\ \\) | | |    | |   ) __ (\n"
+                + " \\ \\__/ /     | |    | | \\   | | |    | |  / (  ) \\ \n"
+                + "  \\    /   ___) (___ | )  \\  | | (____) | ( /    \\ )\n"
+                + "   \\__/    \\_______/ |/    )_) (________) |/      \\|\n";
+
+        System.out.println("____________________________________________________________");
+        System.out.println(logo);
+        System.out.println("Hello! I am your favourite assistant Vinux.");
+        System.out.println("I'm listening, unfortunately.");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Try not to miss me too much ;)");
+        System.out.println("____________________________________________________________");
+    }
+}
+
+
