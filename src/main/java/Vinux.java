169930238ac321fe1efb6d70a6532ea8cddcdf2b
LAPTOP-RK1BUFU5\Indirapratibha <s_indirapratibha@u.nus.edu>
2026-01-28 22:09:40 +0800

    Level-2: store text entered by user and return to them, using arrays

--- src/main/java/Vinux.java
+++ src/main/java/Vinux.java
@@ -4,14 +4,14 @@ public class Vinux {
     public static void main(String[] args) {
         //Level-0: Rename, Greet, Exit
         //logo of the chatbox: VINUX
-        String logo = "           ________   __                         \n"
-                + "|\\      /| \\__  __/  ( (    /| |\\      /| |\\      /|\n"
-                + "| )    ( |    ) (    |  \\  ( | | )    ( | ( \\    / )\n"
-                + "| |    | |    | |    |   \\ | | | |    | |  \\ (__) /\n"
-                + "( (    ) )    | |    | (\\ \\) | | |    | |   ) __ (\n"
-                + " \\ \\__/ /     | |    | | \\   | | |    | |  / (  ) \\ \n"
-                + "  \\    /   ___) (___ | )  \\  | | (____) | ( /    \\ )\n"
-                + "   \\__/    \\_______/ |/    )_) (________) |/      \\|\n";
+        String logo = "              ________   __                         \n"
+                + "   |\\      /| \\__  __/  ( (    /| |\\      /| |\\      /|\n"
+                + "   | )    ( |    ) (    |  \\  ( | | )    ( | ( \\    / )\n"
+                + "   | |    | |    | |    |   \\ | | | |    | |  \\ (__) /\n"
+                + "   ( (    ) )    | |    | (\\ \\) | | |    | |   ) __ (\n"
+                + "    \\ \\__/ /     | |    | | \\   | | |    | |  / (  ) \\ \n"
+                + "     \\    /   ___) (___ | )  \\  | | (____) | ( /    \\ )\n"
+                + "      \\__/    \\_______/ |/    )_) (________) |/      \\|\n";
 
         //print welcome message to user
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -20,12 +20,14 @@ public class Vinux {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("Hello! I am your favourite assistant Vinux.");
-        System.out.println("I'm listening, unfortunately.");
-        System.out.println("____________________________________________________________");
+        System.out.println("I'm listening, unfortunately. Go on.");
+        System.out.println("    ____________________________________________________________");
 
+        //Level-2: create array to store tasks (max 100 tasks)
+        String[] tasks = new String[100];
+        int taskCount = 0; //counter to keep track of how many tasks are stored
 
-        //Level-1: Echo
-        //initialize scanner to read user inout from the console
+        //initialize scanner to read user input from the console
         Scanner scanner = new Scanner(System.in);
         String input = ""; //user input
 
@@ -33,18 +35,30 @@ public class Vinux {
         while (!input.equals("bye")) {
             input = scanner.nextLine(); //read input as long as condition is true
 
-            //echo the input back to the user unless they typed "bye"
+            //process the input unless the user typed "bye"
             if (!input.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("    " + input); //print out user input (echo)
-                System.out.println("____________________________________________________________");
+                System.out.println("    ____________________________________________________________");
+
+                //Level-2: check if user wants to see the list
+                if (input.equals("list")) {
+                    //display all stored tasks
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("    " + (i + 1) + "." + tasks[i]);
+                    }
+                } else {
+                    //store the task in the array
+                    tasks[taskCount] = input;
+                    taskCount ++;
+                    System.out.println("    added: " + input);
+                }
+                System.out.println("    ____________________________________________________________");
             }
         }
 
         //print goodbye message and exit (from Level-0)
         System.out.println("Bye. Try not to miss me too much ;)");
-        System.out.println("||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||");
-        System.out.println("||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||");
+        System.out.println("||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||");
+        System.out.println("||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||");
 
         scanner.close(); //always close the scanner
     }
