ab2bcba240eb46072a544dcc23a8eb616e13273b
LAPTOP-RK1BUFU5\Indirapratibha <s_indirapratibha@u.nus.edu>
2026-01-28 19:36:41 +0800

    Level-1: improved Vinux to echo the user interface back to the user

--- src/main/java/Vinux.java
+++ src/main/java/Vinux.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Vinux {
     public static void main(String[] args) {
+        //Level-0: Rename, Greet, Exit
+        //logo of the chatbox: VINUX
         String logo = "           ________   __                         \n"
                 + "|\\      /| \\__  __/  ( (    /| |\\      /| |\\      /|\n"
                 + "| )    ( |    ) (    |  \\  ( | | )    ( | ( \\    / )\n"
@@ -9,13 +13,40 @@ public class Vinux {
                 + "  \\    /   ___) (___ | )  \\  | | (____) | ( /    \\ )\n"
                 + "   \\__/    \\_______/ |/    )_) (________) |/      \\|\n";
 
-        System.out.println("____________________________________________________________");
+        //print welcome message to user
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println(logo);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("Hello! I am your favourite assistant Vinux.");
         System.out.println("I'm listening, unfortunately.");
         System.out.println("____________________________________________________________");
+
+
+        //Level-1: Echo
+        //initialize scanner to read user inout from the console
+        Scanner scanner = new Scanner(System.in);
+        String input = ""; //user input
+
+        //continuously read and echo user input until "bye" is entered
+        while (!input.equals("bye")) {
+            input = scanner.nextLine(); //read input as long as condition is true
+
+            //echo the input back to the user unless they typed "bye"
+            if (!input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("    " + input); //print out user input (echo)
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        //print goodbye message and exit (from Level-0)
         System.out.println("Bye. Try not to miss me too much ;)");
-        System.out.println("____________________________________________________________");
+        System.out.println("||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||");
+        System.out.println("||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||");
+
+        scanner.close(); //always close the scanner
     }
 }
 
