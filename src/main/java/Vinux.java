ec2bf72b0caf4618181f221f8a25b223aa8fcb7d
LAPTOP-RK1BUFU5\Indirapratibha <s_indirapratibha@u.nus.edu>
2026-01-29 21:21:23 +0800

    Level-4: added the ability to distinguish between type of tasks

--- src/main/java/Vinux.java
+++ src/main/java/Vinux.java
@@ -26,6 +26,8 @@ public class Vinux {
         String[] tasks = new String[100];
         //Level-3: create array to track if tasks are done (true = done, false = not done)
         boolean[] isDone = new boolean[100];
+        //Level-4: create array to track task type: T for ToDos, D for Deadlines, E for Events
+        String[] taskType = new String[100];
         int taskCount = 0; //counter to keep track of how many tasks are stored
 
         //initialize scanner to read user input from the console
@@ -40,7 +42,7 @@ public class Vinux {
             if (!input.equals("bye")) {
                 System.out.println("    ____________________________________________________________");
 
-                //Level-2: check if user wants to see the list
+                //display all the tasks with their status and type
                 if (input.equals("list")) {
                     //display all stored tasks
                     System.out.println("    Why do you have so many things to do?");
@@ -48,7 +50,8 @@ public class Vinux {
                     for (int i = 0; i < taskCount; i++) {
                         //Level-3: display [X] if done, [] if not done
                         String status = isDone[i] ? "[X]" : "[ ]";
-                        System.out.println("    " + (i + 1) + "." + status + tasks[i]);
+                        //display task number, type, status and description
+                        System.out.println("    " + (i + 1) + ".[" + taskType[i] + "]" + status + " " + tasks[i]);
                     }
                 } else if (input.startsWith("mark ")) { //when user marks a task as done
                     //Level-3: mark a task as done
@@ -66,6 +69,52 @@ public class Vinux {
                     isDone[taskNumber] = false; //set condition as false
                     System.out.println("    Aw man! This task is still not done:");
                     System.out.println("        [ ] " + tasks[taskNumber]);
+                } else if (input.startsWith("todo")) {
+                    //Level-4: check if ToDo task (no date/time)
+                    String description = input.substring(5); //everything after "todo"
+                    tasks[taskCount] = description;
+                    taskType[taskCount] = "T";
+                    isDone[taskCount] = false; //task is undone yet
+                    taskCount++;
+                    System.out.println("    Gotcha. I have now added this task:");
+                    System.out.println("        [T][ ] " + description);
+                    System.out.println("    Now you have " + taskCount + " task(s) in the list.");
+
+                } else if (input.startsWith("deadline")) {
+                    //Level-4: check if deadline task
+                    String details = input.substring(9); //everything after "deadline"
+                    //split the input by "/by" to separate descrption and deadline
+                    String[] parts = details.split(" /by "); //get the task
+                    String description = parts[0]; //get task name
+                    String by = parts[1]; //get deadline
+                    tasks[taskCount] = description + " (by: " + by + ")";
+                    taskType[taskCount] = "D";
+                    isDone[taskCount] = false;
+                    taskCount ++;
+                    System.out.println("    Gotcha. I have now added this task:");
+                    System.out.println("        [D][ ] " + description + " (by: " + by + ")");
+                    System.out.println("    Now you have " + taskCount + " task(s) in the list.");
+
+                } else if (input.startsWith("event")) {
+                    //Level-4: check if event task
+                    String details = input.substring(6); //everything after "event"
+                    //split input by "/from" and "/to" to extract start and end time
+                    int fromIndex = details.indexOf(" /from "); //get the from time
+                    int toIndex = details.indexOf(" /to "); //get the to time
+
+                    String description = details.substring(0, fromIndex); //get task name
+                    String from = details.substring(fromIndex + 7, toIndex); //get 'from' date
+                    String to = details.substring(toIndex + 5); //get 'to' date
+
+                    tasks[taskCount] = description + " (from: " + from + " to: " + to + ")";
+                    taskType[taskCount] = "E";
+                    isDone[taskCount] = false;
+                    taskCount ++;
+                    System.out.println("    Gotcha. I have now added this task:");
+                    System.out.println("    [E][ ] " + description + " (from: " + from + " to: " + to + ")");
+                    System.out.println("    Now you have " + taskCount + " task(s) in the list.");
+
+                //default: treat any other input as a simple task
                 } else {
                     //store the task in the array
                     tasks[taskCount] = input;
